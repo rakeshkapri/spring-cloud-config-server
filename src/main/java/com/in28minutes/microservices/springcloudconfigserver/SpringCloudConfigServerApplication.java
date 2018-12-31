7a0665570c57ee2f9dcb12dc8a09ddbc8853c848
rakesh kapri <rakeshkapri@gmail.com>
2018-12-31 13:46:49 +0000

    minor code change

--- src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SpringCloudConfigServerApplication {
-
+public class SpringCloudConfigServerApplication
+	{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
